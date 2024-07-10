569d36886eaa58cd5d3ce9ef9d78168e5e99c30d
Nikita <nikturchinovich@mail.ru>
2024-07-10 23:43:54 +0300

    Update serializers

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -21,7 +21,21 @@ class TaskListSerializer(serializers.ListSerializer):
 
 class TaskSerializer(serializers.ModelSerializer):
     """
-    Сериализатор для модели задачи.
+    Сериализатор для модели задач.
+    """
+
+    class Meta:
+        model = Task
+        fields = '__all__'
+        list_serializer_class = TaskListSerializer
+
+    def to_representation(self, instance):
+        return {'task': super(TaskSerializer, self).to_representation(instance)}
+
+
+class TaskCreateSerializer(TaskSerializer):
+    """
+    Сериализатор для создания модели задачи.
     """
 
     title = serializers.CharField(max_length=255, required=True, allow_null=False, allow_blank=False)
@@ -34,10 +48,18 @@ class TaskSerializer(serializers.ModelSerializer):
     customer = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
     employee = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
 
-    class Meta:
-        model = Task
-        fields = '__all__'
-        list_serializer_class = TaskListSerializer
 
-    def to_representation(self, instance):
-        return {'task': super(TaskSerializer, self).to_representation(instance)}
+class TaskUpdateSerializer(TaskSerializer):
+    """
+    Сериализатор для обновления модели задачи.
+    """
+
+    title = serializers.CharField(max_length=255, required=False, allow_null=False, allow_blank=False)
+    description = serializers.CharField(required=False, allow_null=True, allow_blank=True)
+    status = serializers.CharField(max_length=15, read_only=True)
+    creation_date = serializers.DateTimeField(read_only=True)
+    change_date = serializers.DateTimeField(read_only=True)
+    closing_date = serializers.DateTimeField(read_only=True)
+    report = serializers.CharField(required=False, allow_null=True, allow_blank=True)
+    customer = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
+    employee = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
